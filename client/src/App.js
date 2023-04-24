974942e613f3f556906f2b800af1d7477ff13c1f
Alessandro Cinelli <alessandro.cinelli@orbyta.it>
2023-04-24 23:38:17 +0200

    DIO STRABASTARDO LURIDO CANE

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            PORCODDIO CANE LURIDO BASTARDO
+            PORCODDIO CANE LURIDO BASTARDO SCHIFOSO
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
